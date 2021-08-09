52793a0e68a9bf9a080e3cc8975f23d2962d24bc
alecfarmer <realalecfarmer@gmail.com>
2021-08-08 22:51:32 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -12,10 +12,6 @@ function App() {
           </div>
           <div class="name">James (Alec) Farmer</div>
             <div class="desc">Student & Cybersecurity Enthusiast</div>
-            <div class="sm">
-              <a href="https://www.linkedin.com/in/ja-farmer/" class="fab fa-linkedin"></a>
-              <a href="https://github.com/alecfarmer" class="fab fa-github"></a>
-          </div>
         </div>
       </div>
     </>
